dd6892a913dc81ebcb14a4f0459935e534aa7f02
loleloup <loic.leloup@ulb.ac.be>
2020-11-28 10:40:36 +0100

    init

--- db.c
+++ db.c
@@ -3,21 +3,37 @@
 void db_add(database_t *db, student_t s){
     int i;
     student_t *old = db->data;
-    db->psize += sizeof(student_t)
+    db->psize += sizeof(student_t);
     db->data = (student_t *) malloc(db->psize);
 
     for (i = 0; i < db->lsize; ++i){
-        db->data[i] = old[i]
-    }
+        db->data[i] = old[i];
+    };
 
     db->data[db->lsize] = s;
     db->lsize += 1;
-
+    free(old);
 }
 
 
 void db_delete(database_t *db, student_t *s){
+    int i;
+    student_t *old = db->data;
+    db->psize -= sizeof(student_t);
+    db->data = (student_t *) malloc(db->psize);
 
+    part = (s - db->data)/sizeof(student_t);
+
+    for (i = 0; i < part; ++i){
+        db->data[i] = old[i];
+    };
+
+    for (i = part+1; i < db->lsize; ++i){
+        db->data[i-1] = old[i];
+    };
+
+    db->lsize -= 1;
+    free(old);
 }
 
 
